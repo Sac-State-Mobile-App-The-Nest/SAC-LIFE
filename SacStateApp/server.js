faf3803e6cc12a4134a8cdd06bd58513cb711174
Bryce Chao <brycechao27@gmail.com>
2024-10-19 21:13:19 -0700

    # JIRA uni456 - 31 # Created/Populated DB Tables. This queries from multiple tables from the DB.

--- SacStateApp/server.js
+++ SacStateApp/server.js
@@ -21,9 +21,27 @@ const config = {
 
 //sql connect(config) establishes connection to Azure SQL database using config
 sql.connect(config).then(pool => {
-    return pool.request().query('SELECT * FROM test');
+    return pool.request().query('SELECT * FROM test_students');
 }).then(result => {
     console.log(result);
 }).catch(err => {
     console.error(err);
 });
+
+
+connectAndGetDB();
+
+// make a connection and query from database
+async function connectAndGetDB() {
+    try{
+        console.log('Attempting to connect and retrieve data')
+        var poolConnection = await sql.connect(config);
+        var resultSet = await poolConnection.request().query('SELECT f_name, l_name, serv_name FROM test_students, test_student_tags, test_tag_service, test_campus_services WHERE test_students.std_id = test_student_tags.std_id AND test_student_tags.tag_id = test_tag_service.tag_id AND test_tag_service.service_id = test_campus_services.service_id');
+        console.log('Retrieved data on students and services recommended');
+        console.log(resultSet);
+        //close connection
+        poolConnection.close();
+    } catch (err){
+        console.error(err.message);
+    }
+}
