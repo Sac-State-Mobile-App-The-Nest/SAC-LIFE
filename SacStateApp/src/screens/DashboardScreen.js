3bd5e66e26b48f716d68204fe8b24d56cc623d5a
Justin Rivera <justinrivera@csus.edu>
2024-11-06 19:58:36 -0800

    JIRA#70 (Navigation Bar)
    Implemented a navigator bar and removed some uneccessary text from the Dashboard page. Added two new screens, SettingsScreen and MessengerScreen as empty screens for future navigation bar implementation.

--- SacStateApp/src/screens/DashboardScreen.js
+++ SacStateApp/src/screens/DashboardScreen.js
@@ -2,9 +2,18 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, Button, Alert, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
 import { Calendar } from 'react-native-calendars';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native';
 import ChatWidget from '../components/ChatWidget';
 import backgroundImage from '../assets/logInBackground.jpg'; 
+import { Ionicons } from '@expo/vector-icons';  // Import icons for tabs
+
+//Following are the screens used in the navigator as tabs.
+import ProfileScreen from './ProfileScreen';
+import MessengerScreen from './MessengerScreen';
+import SettingsScreen from './SettingsScreen';
+
+const Tab = createBottomTabNavigator();
 
 const DashboardScreen = () => {
   const navigation = useNavigation();
@@ -125,35 +134,57 @@ const DashboardScreen = () => {
   };
 
   return (
-       <ImageBackground 
-            source={ backgroundImage } 
-            style={styles.background}
-        >
-  <ScrollView contentContainerStyle={styles.scrollViewContainer}>   
-    <View style={styles.goldBackground}>
-      <Text style={styles.header}>Welcome, {userInfo ? `${userInfo.f_name} ${userInfo.m_name} ${userInfo.l_name}`: 'Loading Name'}!</Text>
-      {/* <Text style={styles.header}>Welcome, {user.name}!</Text>
-      <Text style={styles.subHeader}>{user.email}</Text> */}
-      <Text style={styles.details}>Major: {user.major}</Text>
-      <Text style={styles.details}>Year: {user.year}</Text>
-      {/* <Text style={styles.interests}>Interests: {user.interests.join(', ')}</Text> */}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Dashboard Tab') {
+            iconName = 'home';
+          } else if (route.name === 'Profile') {
+            iconName = 'person';
+          } else if (route.name === 'Social Messenger') {
+            iconName = 'people-outline';
+          } else if (route.name === 'Settings') {
+            iconName = 'settings';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#e6b711', // Hornet Yellow
+        tabBarInactiveTintColor: 'grey',
+        tabBarStyle: {
+          backgroundColor: '#021e14', // Redwood Grove Green
+        },
+      })}
+    >
+      {/* Dashboard Tab  (Renamed to Dashboard Tab from Dashboard to avoid confusing behaivor during navigation.) */}
+      <Tab.Screen name= "Dashboard Tab" options={{ headerShown: false, title: 'Dashboard'}}>
+        {() => (
+          <ImageBackground source={backgroundImage} style={styles.background}>
+            <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+              <View style={styles.goldBackground}>
+                <Text style={styles.header}>  Welcome, {userInfo ? `${userInfo.f_name} ${userInfo.m_name} ${userInfo.l_name}` : 'Loading Name'}!</Text>
+                {/* <Text style={styles.header}>Welcome, {user.name}!</Text>
+                <Text style={styles.subHeader}>{user.email}</Text> */}
+                <Text style={styles.details}>Major: {user.major}</Text>
+                <Text style={styles.details}>Year: {user.year}</Text>
+                {/* <Text style={styles.interests}>Interests: {user.interests.join(', ')}</Text> */}
 
-      {/**dev **/}
-      
-      <Text style={styles.subHeader}>Student ID: {userInfo ? `${userInfo.std_id}`: 'Loading ID'}</Text>
-      <View style={styles.serviceContainer}>
-      <Text style={styles.sectionHeader}>Services Available</Text>
-         {userServicesRec.length === 0 ? (
-        <Text style={styles.details}>No services available</Text>
-        ) : (
-        userServicesRec.map((service, index) => (
-            <Text key={index} style={{ fontSize: 14 }}>
-                {service.serv_name}: {service.service_link}
-            </Text>
-        ))
-    )}
-       </View>
-       <View style={styles.calendarContainer}>
+                {/**dev **/}
+
+                <Text style={styles.subHeader}>Student ID: {userInfo ? `${userInfo.std_id}` : 'Loading ID'}</Text>
+                <View style={styles.serviceContainer}>
+                  <Text style={styles.sectionHeader}>Services Available</Text>
+                  {userServicesRec.length === 0 ? (
+                    <Text style={styles.details}>No services available</Text>
+                  ) : (
+                    userServicesRec.map((service, index) => (
+                      <Text key={index} style={{ fontSize: 14 }}>
+                        {service.serv_name}: {service.service_link}
+                      </Text>
+                    ))
+                  )}
+                </View>
+                <View style={styles.calendarContainer}>
                 <Calendar
                     onDayPress={(day) => {
                         console.log('selected day', day);
@@ -165,35 +196,38 @@ const DashboardScreen = () => {
                     }}
                     style={styles.calendar}
                 />
-            </View>
+              </View>
+
+                <Text style={styles.sectionHeader}>Notifications</Text>
+                <FlatList data={notifications}
+                 keyExtractor={(item) => item.id} 
+                 renderItem={renderNotification} />
+
+                <TouchableOpacity style={styles.button} 
+                onPress={handleWellBeingCheckIn}>
+
+                  <Text style={{ color: 'white' }}>Check In on Well-Being</Text>
+                </TouchableOpacity>
+
+                <View style={stylesC.chatWidgetContainer}>
+                  <ChatWidget />
+                </View>
+              </View>
+            </ScrollView>
+          </ImageBackground>
+        )}
+      </Tab.Screen>
 
 
-      <Text style={styles.sectionHeader}>Notifications</Text>
-      <FlatList
-        data={notifications}
-        keyExtractor={(item) => item.id}
-        renderItem={renderNotification}
-      />
+      {/* Profile Tab */}
+      <Tab.Screen name="Profile" component={ProfileScreen} />
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={handleWellBeingCheckIn}
-      >
-        <Text style={{ color: 'white' }}>Check In on Well-Being</Text>
-      </TouchableOpacity>
+      {/* Messenger Tab */}
+      <Tab.Screen name="Social Messenger"  component={MessengerScreen} />
 
-      <Text style={styles.sectionHeader}>Recent Activities</Text>
-      <FlatList
-        data={activities}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
-      />
-      <View style={stylesC.chatWidgetContainer}>
-        <ChatWidget />
-      </View>
-    </View>
-    </ScrollView>
-    </ImageBackground>
+      {/* Settings Tab */}
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 };
 
