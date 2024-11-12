3c5c73f5e08fdf2914e45ef7ca1d09010cacacf5
JustBacon <christianbuco17@gmail.com>
2024-11-12 14:24:33 -0800

    #63 Store profile creation answers to SQL database

--- backend-api/routes/students.js
+++ backend-api/routes/students.js
@@ -5,11 +5,6 @@ const sql = require('mssql');
 
 // Export a function that accepts the poolPromise
 module.exports = function(poolPromise) {
-
-  //Posting to test student tags table
-  router.post('/profile', async (req, res) => {
-
-  });
   
   // Get all students
   router.get('/', async (req, res) => {
@@ -72,6 +67,52 @@ module.exports = function(poolPromise) {
       console.error('SQL error', err);
       res.status(500).json({ message: 'Backend server error' });
     }
+  });
+
+  //Posting to test student tags table
+  router.post('/profile-answers', async (req, res) => {
+    const { specificAnswers } = req.body;
+    // replace with req.studentId
+    const studentId = 15;
+
+    const { question1, question2, question3 } = specificAnswers;
+
+    try {
+      const pool = await poolPromise;
+
+      tags = await pool.request()
+        .input('tagName1', sql.VarChar, question1)
+        .input('tagName2', sql.VarChar, question2)
+        .input('tagName3', sql.VarChar, question3)
+        .query('SELECT tag_id, tag_name FROM test_tags WHERE tag_name IN (@tagName1, @tagName2, @tagName3)');
+
+      const tagMap = tags.recordset.reduce((acc, tag) => {
+        acc[tag.tag_name] = tag.tag_id;
+        return acc;
+      }, {});
+
+      // Insert users
+      await pool.request()
+        .input('std_id', sql.Int, studentId)
+        .input('tagId1', sql.Int, tagMap[question1])
+        .query('INSERT INTO test_student_tags (std_id, tag_id) VALUES (@std_id, @tagId1)');
+
+      await pool.request()
+        .input('std_id', sql.Int, studentId)
+        .input('tagId2', sql.Int, tagMap[question2])
+        .query('INSERT INTO test_student_tags (std_id, tag_id) VALUES (@std_id, @tagId2)');
+
+      await pool.request()
+        .input('std_id', sql.Int, studentId)
+        .input('tagId3', sql.Int, tagMap[question3])
+        .query('INSERT INTO test_student_tags (std_id, tag_id) VALUES (@std_id, @tagId3)');
+
+      res.status(200).json({ message: 'Profile creation made'});
+    } catch (err) {
+      console.error('SQL error:', err);
+      res.status(500).send('Server Error');
+    }
+
   });
 
   // DELETE a student by studentId
