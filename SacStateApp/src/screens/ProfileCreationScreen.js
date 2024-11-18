e89a22227486be49d3e8b8e7452280c212055a4a
Nicholas Lewis <145616287+nickLewisCSUS@users.noreply.github.com>
2024-11-17 20:32:57 -0800

    Fixed issues with rendering and Progress bar Error

--- SacStateApp/src/screens/ProfileCreationScreen.js
+++ SacStateApp/src/screens/ProfileCreationScreen.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, TouchableOpacity, TextInput, Alert, Keyboard, ImageBackground, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, TextInput, Alert, ImageBackground, Dimensions } from 'react-native';
 import ModalSelector from 'react-native-modal-selector';
-import { ProgressBar } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import backgroundImage from '../assets/logInBackground.jpg';
 import majorList from '../assets/majorList.json';
@@ -70,14 +69,13 @@ const ProfileCreation = () => {
     const [lastName, setLastName] = useState("");
     const [isCompleted, setIsCompleted] = useState(false);
     const navigation = useNavigation();
-   
 
     const questions = [
         new Question(0, "Please enter your name details (First, Middle Initial (optional), Last):", "text"),
         new Question(1, "What type of student are you?", "radio", ["New Student", "Transfer Student", "Re-entry Student"]),
-        new Question(2, "What is your major?", "dropdown", majorList.parse()["major"]),
+        new Question(2, "What is your major?", "dropdown", majorList["major"]),
         new Question(3, "What academic year are you in?", "radio", ["Freshman", "Sophomore", "Junior", "Senior+", "Graduate"]),
-        new Question(4, "Which clubs are you apart of or interest in?", "multiDropdown", clubList.parse()["club"]),
+        new Question(4, "Which clubs are you a part of or interested in?", "multiDropdown", clubList["club"]),
         new Question(5, "What type of campus events are you interested in?", "checkbox", ["Academic Workshops", "Social Events", "Sports", "Volunteering"]),
         new Question(6, "Which areas of support would you find most helpful?", "checkbox", ["Academic Advising", "Career Counseling", "Mental Health Resources", "Financial Aid"]),
         new Question(7, "What are your academic goals?", "checkbox", ["Achieve high grades", "Get hands-on experience", "Build a professional network", "Plan for further education"])
@@ -101,8 +99,8 @@ const ProfileCreation = () => {
     };
 
     const renderQuestion = (question) => {
-        switch(question.inputType){
-            case 'text': 
+        switch (question.inputType) {
+            case "text":
                 return (
                     <View>
                         <TextInput
@@ -139,7 +137,7 @@ const ProfileCreation = () => {
             case "dropdown":
                 return (
                     <ModalSelector
-                        data = {question.options}
+                        data={question.options}
                         initValue="Select your major"
                         onChange={(option) => setSelectedMajor(option.label)}
                         style={styles.pickerContainer}
@@ -147,10 +145,10 @@ const ProfileCreation = () => {
                         selectTextStyle={styles.pickerText}
                     />
                 );
-            case "multiDropdown": 
+            case "multiDropdown":
                 return (
                     <ModalSelector
-                        data =  {question.options}
+                        data={question.options}
                         initValue="Select the clubs"
                         onChange={(option) => setSelectedClub(option.label)}
                         style={styles.pickerContainer}
@@ -158,7 +156,7 @@ const ProfileCreation = () => {
                         selectTextStyle={styles.pickerText}
                     />
                 );
-            default: 
+            default:
                 return (
                     <TextInput
                         style={styles.input}
@@ -166,11 +164,8 @@ const ProfileCreation = () => {
                         onChangeText={(text) => profileCreationManager.handleAnswer(question.id, text, currentQuestion)}
                     />
                 );
-            
-    }
-}
-        
-       
+        }
+    };
 
     const handleNextPress = () => {
         if (currentQuestion === 0) {
@@ -179,102 +174,97 @@ const ProfileCreation = () => {
                 return;
             }
             profileCreationManager.handleAnswer(0, { firstName, middleInitial, lastName }, currentQuestion);
-        }    
-    }
-          profileCreationManager.goToNext(currentQuestion);
-};
+        }
+        profileCreationManager.goToNext(currentQuestion);
+    };
 
-const renderCompletionScreen = () => {(
-    <View style={styles.completionContainer}>
-        <Text style={styles.completionText}>You have finished customizing your personal profile!</Text>
-        <TouchableOpacity style={styles.largeButton} onPress={restartProfileCreation}>
-            <Text style={styles.largeButtonText}>Redo</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-            style={styles.largeButton}
-            onPress={() => navigation.navigate('Home')}
-        >
-            <Text style={styles.largeButtonText}>Return to Home</Text>
-        </TouchableOpacity>
-    </View>
-);
-
-return (
-    <ImageBackground source={backgroundImage} style={styles.background}>
-        <View style={styles.overlay}>
-            <ScrollView contentContainerStyle={styles.container}>
-                {isCompleted ? (
-                    renderCompletionScreen()
-                ) : (
-                    <>
-                        <ProgressBar progress={Math.round((currentQuestion + 1) / questions.length)} color="#B3A369" style={styles.progressBar} />
-                        <Text style={styles.heading}>Question {currentQuestion + 1} of {questions.length}</Text>
-                        <View style={styles.box}>
-                            <Text style={styles.questionText}>{questions[currentQuestion].text}</Text>
-                            {renderQuestion(questions[currentQuestion])}
-                        </View>
-                        <View style={styles.navigationButtons}>
-                            <TouchableOpacity style={styles.button} onPress={() => profileCreationManager.goToPrevious(currentQuestion)} disabled={currentQuestion === 0}>
-                                <Text style={styles.buttonText}>Previous</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={styles.button}
-                                onPress={() => {
-                                    if (currentQuestion === questions.length - 1) {
-                                        completeProfileCreation();
-                                    } else {
-                                        handleNextPress();
-                                    }
-                                }}
-                            >
-                                <Text style={styles.buttonText}>{currentQuestion < questions.length - 1 ? 'Next' : 'Submit'}</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </>
-                )}
-            </ScrollView>
+    const renderCompletionScreen = () => (
+        <View style={styles.completionContainer}>
+            <Text style={styles.completionText}>You have finished customizing your personal profile!</Text>
+            <TouchableOpacity style={styles.largeButton} onPress={restartProfileCreation}>
+                <Text style={styles.largeButtonText}>Redo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.largeButton}
+                onPress={() => navigation.navigate("Home")}
+            >
+                <Text style={styles.largeButtonText}>Return to Home</Text>
+            </TouchableOpacity>
         </View>
-    </ImageBackground>
-);
-};
+    );
 
+    return (
+        <ImageBackground source={backgroundImage} style={styles.background}>
+            <View style={styles.overlay}>
+                <ScrollView contentContainerStyle={styles.container}>
+                    {isCompleted ? (
+                        renderCompletionScreen()
+                    ) : (
+                        <>
+                            <Text style={styles.heading}>Question {currentQuestion + 1} of {questions.length}</Text>
+                            <View style={styles.box}>
+                                <Text style={styles.questionText}>{questions[currentQuestion].text}</Text>
+                                {renderQuestion(questions[currentQuestion])}
+                            </View>
+                            <View style={styles.navigationButtons}>
+                                <TouchableOpacity style={styles.button} onPress={() => profileCreationManager.goToPrevious(currentQuestion)} disabled={currentQuestion === 0}>
+                                    <Text style={styles.buttonText}>Previous</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={styles.button}
+                                    onPress={() => {
+                                        if (currentQuestion === questions.length - 1) {
+                                            completeProfileCreation();
+                                        } else {
+                                            handleNextPress();
+                                        }
+                                    }}
+                                >
+                                    <Text style={styles.buttonText}>{currentQuestion < questions.length - 1 ? "Next" : "Submit"}</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </>
+                    )}
+                </ScrollView>
+            </View>
+        </ImageBackground>
+    );
+};
 
 const styles = StyleSheet.create({
-    background: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-    overlay: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)', width: '100%' },
-    container: { padding: 20, backgroundColor: 'transparent', flex: 1 },
-    progressBar: { width: '100%', marginVertical: 20 },
-    heading: { fontSize: 26, fontWeight: 'bold', color: 'white', marginBottom: 15, textAlign: 'center' },
-    box: { 
-        backgroundColor: '#c4b581', 
-        borderRadius: 10, 
-        padding: 20, 
-        width: '80%', 
-        minHeight: height * 0.2, 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        alignSelf: 'center',
-        shadowColor: '#000', 
-        shadowOffset: { width: 0, height: 2 }, 
-        shadowOpacity: 0.25, 
-        shadowRadius: 3.5, 
-        elevation: 5, 
-        marginVertical: 10 
+    background: { flex: 1, justifyContent: "center", alignItems: "center" },
+    overlay: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "rgba(0, 0, 0, 0.5)", width: "100%" },
+    container: { padding: 20, backgroundColor: "transparent", flex: 1 },
+    heading: { fontSize: 26, fontWeight: "bold", color: "white", marginBottom: 15, textAlign: "center" },
+    box: {
+        backgroundColor: "#c4b581",
+        borderRadius: 10,
+        padding: 20,
+        width: "80%",
+        minHeight: height * 0.2,
+        justifyContent: "center",
+        alignItems: "center",
+        alignSelf: "center",
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        elevation: 5,
+        marginVertical: 10,
     },
-    questionText: { fontSize: 20, color: '#043927', fontWeight: '500', textAlign: 'center', marginBottom: 20 },
-    optionButton: { padding: 14, backgroundColor: '#043927', marginVertical: 5, borderRadius: 8, width: '90%', alignItems: 'center' },
-    optionText: { fontSize: 18, color: '#FFFFFF', fontWeight: '500' },
-    input: { padding: 12, borderWidth: 1, borderColor: 'gray', borderRadius: 5, backgroundColor: 'white', marginBottom: 10, width: '90%' },
-    pickerContainer: { width: '90%', backgroundColor: 'white', borderRadius: 8, borderColor: '#043927', borderWidth: 1, paddingHorizontal: 10, paddingVertical: 12 },
-    pickerText: { color: '#043927', fontSize: 18, textAlign: 'center' },
-    navigationButtons: { flexDirection: 'row', justifyContent: 'space-between', marginTop: 20, width: '90%', alignSelf: 'center' },
-    button: { paddingVertical: 12, paddingHorizontal: 30, backgroundColor: '#043927', borderRadius: 12, width: '45%', alignItems: 'center' },
-    buttonText: { color: 'white', fontSize: 16, fontWeight: '500', textAlign: 'center' },
-    completionContainer: { alignItems: 'center', padding: 30 },
-    completionText: { fontSize: 24, fontWeight: 'bold', color: 'white', marginBottom: 25, textAlign: 'center' },
-    largeButton: { paddingVertical: 12, paddingHorizontal: 40, backgroundColor: '#043927', borderRadius: 10, width: '80%', marginVertical: 10, alignItems: 'center' },
-    largeButtonText: { color: 'white', fontSize: 18, fontWeight: '600', alignItems: 'center', textAlign: 'center'},
+    questionText: { fontSize: 20, color: "#043927", fontWeight: "500", textAlign: "center", marginBottom: 20 },
+    optionButton: { padding: 14, backgroundColor: "#043927", marginVertical: 5, borderRadius: 8, width: "90%", alignItems: "center" },
+    optionText: { fontSize: 18, color: "#FFFFFF", fontWeight: "500" },
+    input: { padding: 12, borderWidth: 1, borderColor: "gray", borderRadius: 5, backgroundColor: "white", marginBottom: 10, width: "90%" },
+    pickerContainer: { width: "90%", backgroundColor: "white", borderRadius: 8, borderColor: "#043927", borderWidth: 1, paddingHorizontal: 10, paddingVertical: 12 },
+    pickerText: { color: "#043927", fontSize: 18, textAlign: "center" },
+    navigationButtons: { flexDirection: "row", justifyContent: "space-between", marginTop: 20, width: "90%", alignSelf: "center" },
+    button: { paddingVertical: 12, paddingHorizontal: 30, backgroundColor: "#043927", borderRadius: 12, width: "45%", alignItems: "center" },
+    buttonText: { color: "white", fontSize: 16, fontWeight: "500", textAlign: "center" },
+    completionContainer: { alignItems: "center", padding: 30 },
+    completionText: { fontSize: 24, fontWeight: "bold", color: "white", marginBottom: 25, textAlign: "center" },
+    largeButton: { paddingVertical: 12, paddingHorizontal: 40, backgroundColor: "#043927", borderRadius: 10, width: "80%", marginVertical: 10, alignItems: "center" },
+    largeButtonText: { color: "white", fontSize: 18, fontWeight: "600", alignItems: "center", textAlign: "center" },
 });
 
 export default ProfileCreation;
-
