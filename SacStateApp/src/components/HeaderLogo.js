0d15cd95078f2b2d63b2f81d3b40068731cabd73
Nicholas Lewis <145616287+nickLewisCSUS@users.noreply.github.com>
2024-11-21 13:54:44 -0800

    Revert "Refractered code for dahsboard UI to make it mroe organized"
    This reverts commit 401c09e550d971467eec2ae6255d7f10a9c33c3a.

--- SacStateApp/src/components/HeaderLogo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Image } from 'react-native';
-import styles from '../styles/DashboardStyles';
-import logo from '../assets/sac-state-logo.png';
-
-const HeaderLogo = () => <Image source={logo} style={styles.logo} />;
-
-export default HeaderLogo;
