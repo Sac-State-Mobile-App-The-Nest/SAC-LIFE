b2cf464cd3fa4103678eade2ede36cec1683713d
brycee916 <brycechao27@gmail.com>
2025-02-25 00:54:37 -0800

    UNI456-86: Limited the support services shown to 3 and added a new page where users can search for their recommended support services.

--- SacStateApp/src/DashboardComponents/ServicesList.js
+++ SacStateApp/src/DashboardComponents/ServicesList.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Linking } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import styles from '../DashboardStyles/ServicesStyles';
 
 const ServicesList = ({ services }) => {
+
+  //Randomize the services and show only 3 at a time, after clicking "Search Your Services, it'll navigate to all of your services page"
+  const [displayedServices, setDisplayedServices] = useState([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (services.length > 0) {
+      shuffleAndSetServices();
+    }
+  }, [services]);
+
+  const shuffleAndSetServices = () => {
+    const shuffled = [...services].sort(() => Math.random() - 0.5);
+    setDisplayedServices(shuffled.slice(0, 3));
+  };
+
   const handlePress = (link) => {
     if (link) {
       Linking.openURL(link).catch((err) =>
@@ -15,9 +33,11 @@ const ServicesList = ({ services }) => {
     <View style={styles.serviceContainer}>
       <Text style={styles.servicesHeader}>Find Your Support</Text>
       {services.length === 0 ? (
-        <Text style={styles.details}>Want to explore services? Update your preferences anytime!</Text>
+        <Text style={styles.details}>
+          Want to explore services? Update your preferences anytime!
+        </Text>
       ) : (
-        services.map((service, index) => (
+        displayedServices.map((service, index) => (
           <TouchableOpacity
             key={index}
             style={styles.serviceBox}
@@ -26,6 +46,17 @@ const ServicesList = ({ services }) => {
             <Text style={styles.serviceTitle}>{service.serv_name}</Text>
           </TouchableOpacity>
         ))
+      )}
+      {services.length > 3 && (
+        <TouchableOpacity
+          style={styles.serviceBoxShowMore}
+          onPress={() => navigation.navigate('AllServices', { services })}
+        >
+          <View style={styles.searchContainer}>
+            <Ionicons name="search" size={24} style={styles.searchIcon} />
+            <Text style={styles.serviceTitleShowMore}>Search Your Services</Text>
+          </View>
+        </TouchableOpacity>
       )}
     </View>
   );
