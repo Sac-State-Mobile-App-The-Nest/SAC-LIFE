f5864e59a08ea515aa3f353144f468417ac68614
Nicholas Lewis <145616287+nickLewisCSUS@users.noreply.github.com>
2025-03-18 20:15:50 -0700

    QuickFix/add-azure-url: Azure url added. It only uses azure url for building the app and it uses your local IP for testing and development of the expo app.

---
+++ SacStateApp/src/apiConfig.js
@@ -0,0 +1,7 @@
+import { DEV_BACKEND_SERVER_IP, DEV_BACKEND_PORT, PROD_BACKEND_URL } from '@env';
+// apiConfig.js
+const BASE_URL = __DEV__
+  ? `http://${DEV_BACKEND_SERVER_IP}:${DEV_BACKEND_PORT}` // Local server for dev
+  : PROD_BACKEND_URL;  // Production Azure URL
+
+export default BASE_URL;
--- SacStateApp/src/screens/LogInScreen.js
+++ SacStateApp/src/screens/LogInScreen.js
@@ -8,6 +8,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { Alert } from 'react-native';
 import styles from '../LoginStyles/LoginStyles';
+import BASE_URL from '../apiConfig.js';
 
 const LogInScreen = () => {
     const navigation = useNavigation(); 
@@ -20,13 +21,13 @@ const LogInScreen = () => {
     // Login function
     const login = async () => {
         try {
-            const response = await axios.post(`http://${process.env.DEV_BACKEND_SERVER_IP}:5000/api/login_info/login`, { username, password, });   // Will add IP's to a .env file in the future
+            const response = await axios.post(`${BASE_URL}/api/login_info/login`, { username, password, });   // Will add IP's to a .env file in the future
             const token = response.data.accessToken;
 
             await AsyncStorage.setItem('token', token);
             await AsyncStorage.setItem('username', username);
 
-            const booleanResponse = await axios.get(`http://${process.env.DEV_BACKEND_SERVER_IP}:5000/api/login_info/check-login-bool`, {
+            const booleanResponse = await axios.get(`${BASE_URL}/api/login_info/check-login-bool`, {
                 headers: { Authorization: `Bearer ${token}` },
             });
 
@@ -59,11 +60,7 @@ const LogInScreen = () => {
         }
         
         setLoading(true);
-        // Simulate a login process
-        setTimeout(() => {
-            setLoading(false);
-            login();
-        }, 2000);
+        login();
     };
 
     const handleSkip = () => {
