581ecb073fb41fa41238d356e9fb4692716fcdb2
Bryce Chao <brycechao27@gmail.com>
2025-03-25 15:52:13 -0700

    QuickFix: connects to backend now

--- SacStateApp/src/apiConfig.js
+++ SacStateApp/src/apiConfig.js
@@ -1,7 +1,7 @@
 import { DEV_BACKEND_SERVER_IP, DEV_BACKEND_PORT, PROD_BACKEND_URL } from '@env';
 // apiConfig.js
 const BASE_URL = __DEV__
-  ? `http://${DEV_BACKEND_SERVER_IP}:${DEV_BACKEND_PORT}` // Local server for dev
+  ? `http://${process.env.DEV_BACKEND_SERVER_IP}:${process.env.DEV_BACKEND_PORT}` // Local server for dev
   : PROD_BACKEND_URL;  // Production Azure URL
 
 export default BASE_URL;
